c31c751d09fbaa82dc35e43bdbafee371c8c8326
Bleu <kenxijay@gmail.com>
2024-03-03 15:58:53 +0100

    Mar03

--- palindrome.js
+++ palindrome.js
@@ -1,9 +1,12 @@
 
 function isPalindrome(word = 'racecar') {
+    //The split('') method is used to split the input word into an array of characters.
+    //The reverse() method is used to reverse the order of the elements in the array.
+    //The join('') method is used to combine the elements in the array back into a single string.
     const reversedWord = word.split('').reverse().join('');
-    return word === reversedWord;
+    return word === reversedWord;   //The 'return' statement checks if the reversed word is the same as the original word. If they are the same, then the function returns true. Otherwise, it returns false.
 }
 
-console.log(isPalindrome); // true
+console.log(isPalindrome); // function Type
 console.log(isPalindrome("racecar")); // true
 console.log(isPalindrome("Big")); // false
